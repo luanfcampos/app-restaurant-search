385664a81c34a9c0ff69f4633085be61f520883f
Luan Ferreira Campos <86481756+luanfcampos@users.noreply.github.com>
2021-08-24 11:06:51 -0300

    Ajustando campo de pesquisa
    Correções no layout do campo de pesquisa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,33 @@
-import React, {useState} from 'react';
-import TextField, { Input } from '@material/react-text-field';
+import React, {useState} from 'react'
+import TextField, { Input } from '@material/react-text-field'
+import MaterialIcon from '@material/react-material-icon'
 
-import { Container } from './styles';
 import logo from '../../assets/logo.svg'
+
+import { Container, Search, Logo, Wrapper, Map} from './styles'
 
 const Home = () => {
     const [inputValue, setImputValue] = useState('')
 
     return (
-    <Container>
-    <Search><img src={logo} alt="logo do site" /></Search>
-    <TextField
-          label='Pesquisar'
-          outlined
-          // onTrailingIconSelect={() => this.setState({value: ''})}
-          // trailingIcon={<MaterialIcon role="button" icon="delete"/>}
-        ><Input
-           value={inputValue}
-           onChange={(e) =>  setImputValue(e.target.value)
-            />
-        </TextField>
-    </Container>
+    <Wrapper>
+        <Container>
+            <Search>
+                <Logo src={logo} alt="logo do site" />
+                <TextField
+                    label='Pesquisar'
+                    outlined
+                    onTrailingIconSelect={() => this.setState({value: ''})}
+                    trailingIcon={<MaterialIcon role="button" icon="search"/>}
+                    >
+                    <Input value={inputValue} onChange={(e) =>  setImputValue(e.target.value)} />
+                </TextField>
+            </Search>
+        </Container>
+        <Map />
+    </Wrapper>
     )
-};
+}
   
-export default Home;
+export default Home
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,17 +1,32 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
+
 export const Container = styled.aside`
     background-color: ${(props) => props.theme.colors.background};
     width: 360px;
     height: 100vh;
-    overflow-y: scroll;
-`;
+    overflow-y: auto;
+`
 
 export const Search = styled.section`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    background-color: #fff;
+    background-color: #ffffff;
     padding: 16px;
 
+`
+
+export const Logo = styled.img`
+    margin-bottom: 15px;
+`
+
+export const Map = styled.map`
+    background-color: red;
+    width: 500px;
 `
